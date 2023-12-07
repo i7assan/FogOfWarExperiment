31c2387adf167be20b07cb755ac5252428de2714
i7assan <7assanassiri@gmail.com>
2023-12-07 11:55:25 +0300

    Init

---
+++ FogOfWarExperiment/FogOfWarExperiment/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FogOfWarExperiment
+//
+//  Created by i7assan on 07/12/2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+
+    var body: some View {
+        ZStack{
+            Map()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+      
+}
---
+++ FogOfWarExperiment/FogOfWarExperiment/FogOfWarExperimentApp.swift
@@ -0,0 +1,21 @@
+//
+//  FogOfWarExperimentApp.swift
+//  FogOfWarExperiment
+//
+//  Created by i7assan on 07/12/2023.
+//
+
+import SwiftUI
+
+
+@main
+struct FogOfWarExperimentApp: App {
+
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+    }
+}
